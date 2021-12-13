f76862c85d6e728ea67dadeeae2e1615543db302
Rockycta <50723705+Rockycta@users.noreply.github.com>
2021-12-13 12:11:00 +0530

    Add files via upload

---
+++ com.sfm.in - git-done/Test.java
@@ -0,0 +1,21 @@
+package com.sfm.in.beans;
+
+import java.net.URI;
+import java.net.URL;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+			XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(new ClassPathResource("application-context.xml"));
+			Object cal = (Object)xmlBeanFactory.getBean(Task.class);
+			System.out.println(cal);
+			
+	}
+	
+	
+}
